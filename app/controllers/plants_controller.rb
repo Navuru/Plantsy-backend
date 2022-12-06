dc3c61bd6b3e808d406fd2220f03f2fc8afff00b
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-06 16:47:58 +0300

    Create routes for plants and controller actions

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,42 @@
 class PlantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+
+    def index
+        plants = Plant.all
+        render json: plants, status: :ok
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        render json: plant,status: :ok
+    end
+
+    def create
+        plant = Plant.create!(plant_params)
+        render json: plant, status: :created
+    end
+
+    def update
+        plant = Plant.find(params[:id])
+        plant.update!( plant_params)
+        render json: plant, status: :created
+    end
+
+    def destroy
+        plant = Plant.find(params[:id])
+        plant.delete
+        head :no_content
+    end
+
+
+    private
+    def record_not_found
+        render json: {error: "Record not found"}, status: :not_found
+    end
+
+    def plant_params
+        params.permit(:name, :description, :image_url, :likes)
+    end
+    
 end
