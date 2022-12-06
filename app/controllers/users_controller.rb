83a3b2937963546b4668ac5beb34146140e26084
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-06 17:07:38 +0300

    Update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+    skip_before_action :authorize, only: [:create]
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        if user
+        render json: user
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username,:password_confirmation,:password)
+    end
 end
