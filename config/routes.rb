7db58ba7efc4fa32142200f8741b2416be99649b
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-06 17:04:49 +0300

    Update routes to login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :plants
   resources :reviews, only: [:create, :index, :destroy]
+
+  post "/login",to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  post "/signup",to: "users#create"
+  get "/me", to: "users#show"
 end
