d2c896707e80cb71dfee1d04f07d2f63ca286a88
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-06 17:02:06 +0300

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+      before_action :authorize
+    
+    
+      private
+      def invalid_record(e)
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+      end
+    
+      def authorize
+        user = User.find_by(id: session[:user_id])
+        render json: {errors:["Not authorized"]}, status: :unauthorized unless user
+      end
 end
