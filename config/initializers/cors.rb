57e9be1e180ea25a3e4d01d94b5936b549d5adc9
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-11 20:18:01 +0300

    Edit cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,4 +22,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
+
+    allow do
+      origins "https://plantsy-production-7d90.up.railway.app"
+  
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
