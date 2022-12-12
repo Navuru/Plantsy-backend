07a6e8cc8a3fa537c7855cff419b231218d6170c
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-12-12 06:57:02 +0300

    Update authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::API
         render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
       end
     
-      def authorize
-        user = User.find_by(id: session[:user_id])
-        render json: {errors:["Not authorized"]}, status: :unauthorized unless user
-      end
+      # def authorize
+      #   user = User.find_by(id: session[:user_id])
+      #   render json: {errors:["Not authorized"]}, status: :unauthorized unless user
+      # end
 end
